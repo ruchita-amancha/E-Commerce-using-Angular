3c30ed7eeec3c86583e86fb40611608be9e43239
Ruchita Amancha <amancharuchita@gmail.com>
2024-04-25 15:49:56 +0530

    Populate Update Product Form

--- src/app/seller-update-product/seller-update-product.component.ts
+++ src/app/seller-update-product/seller-update-product.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../services/product.service';
+import { product } from 'src/dataTypes';
 
 @Component({
   selector: 'app-seller-update-product',
@@ -6,8 +9,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./seller-update-product.component.css']
 })
 export class SellerUpdateProductComponent {
-submit(arg0: any) {
-throw new Error('Method not implemented.');
-}
+
+  constructor(private route:ActivatedRoute,private product:ProductService){}
+
+  productData:undefined|product
+
+  ngOnInit():void{
+    let productId=this.route.snapshot.paramMap.get('id')
+    // console.warn(productId);
+    productId && this.product.getProduct(productId).subscribe((data)=>{
+      console.warn(data);
+      this.productData=data
+    })
+  }
+
+  submit(data:any) {
+
+  }
 
 }
