4dd25aacc43be04a6641e087a8ee8a9fb42b6fb7
Ruchita Amancha <amancharuchita@gmail.com>
2024-04-25 12:30:50 +0530

    UI for Update Product

--- src/app/seller-home/seller-home.component.ts
+++ src/app/seller-home/seller-home.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { ProductService } from '../services/product.service';
 import { product } from 'src/dataTypes';
-import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { faTrash,faEdit } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-seller-home',
   templateUrl: './seller-home.component.html',
@@ -12,6 +12,7 @@ export class SellerHomeComponent {
   productList: undefined | product[];
   productMessage=""
   icon=faTrash;
+  editIcon=faEdit;
   constructor(private product: ProductService) {}
 
   ngOnInit(): void {
